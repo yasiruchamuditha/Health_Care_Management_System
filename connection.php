a4e2215bc0408a97e0c0251c4b6070900d697d84
Yasiru Chamuditha <109359904+yasiruchamuditha@users.noreply.github.com>
2023-07-02 12:33:20 +0530

    02/07/2023
    02/07/2023

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+  //connection
+    //$con = mysqli_connect("dbserver", "dbuser","dbpassword","dbname" );
+    $con = mysqli_connect("localhost:3306", "root","DdCya995142@4681","Healthcare" );
+
+ //checking the connection
+    if(mysqli_connect_errno())
+    {
+    	die('Database Connection Failed');
+    }
+
+?>
