11bb2fc909087c63151adba9d15d00bb5c555373
Yasiru Chamuditha <109359904+yasiruchamuditha@users.noreply.github.com>
2023-07-30 17:14:09 +0530

    30,07,2023 5.14 P.M.

--- M_Connection.php
+++ M_Connection.php
@@ -1,8 +1,7 @@
 <?php
-
   //connection
-    //$con = mysqli_connect("dbserver", "dbuser","dbpassword","dbname" );
-    $con = mysqli_connect("localhost:3306", "root","DdCya995142@4681","helthcare" );
+  //$con = mysqli_connect("dbserver", "dbuser","dbpassword","dbname" );
+  $con = mysqli_connect("localhost:3306", "root","DdCya995142@4681","helthcare" );
 
  //checking the connection
     if(mysqli_connect_errno())
